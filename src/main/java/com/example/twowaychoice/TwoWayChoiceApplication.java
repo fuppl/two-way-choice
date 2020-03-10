0a055645b320340ac864ab4210ccf7fe46f45bd3
fuppl <2766700872@qq.com>
2020-03-10 15:02:18 +0800

    initialized

---
+++ src/main/java/com/example/twowaychoice/TwoWayChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.twowaychoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoWayChoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoWayChoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twowaychoice/TwoWayChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twowaychoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoWayChoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
